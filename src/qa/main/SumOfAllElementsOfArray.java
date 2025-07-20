71a817bd5b6d64405ab9c38535de424e38e14ad6
Umair Boota <umair.boota@venturedive.com>
2025-07-20 19:37:24 +0500

    learning arrays

---
+++ src/qa/main/SumOfAllElementsOfArray.java
@@ -0,0 +1,16 @@
+package qa.main;
+
+public class SumOfAllElementsOfArray {
+
+    public void sumOfAllElementsOfArray(int[] arr){
+
+
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
