77b97c6b4577c58132cf5cca2e0226d89d8f0947
Umair Boota <umair.boota@venturedive.com>
2025-09-27 16:16:00 +0500

    refactoring

--- src/qa/main/Arrays.java
+++ src/qa/main/Arrays.java
@@ -3,16 +3,33 @@ package qa.main;
 public class Arrays {
 
 
-    public void largestElementInArray(){
+    public int  largestElementInArray(){
 
 
         int[] arr = {4, 9, 56, 8, 4, 3, 0};
 
+        int largest = arr[0];
+
+        for (int i = 0; i < arr.length-1; i++) {
+            if (largest < arr[i]){
+                largest = arr[i];
+            }
+        }
+
+        return largest;
 
         }
 
 
     public static void main(String[] args) {
+
+        Arrays test = new Arrays();
+
+       int LargestNumber = test.largestElementInArray();
+
+        System.out.println("Largest Number: " + LargestNumber);
+
+
 
 
 
