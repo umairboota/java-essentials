5433c5a35b45090c35b070488cc35789f9cc8293
Umair <mumairboota007@gmail.com>
2025-09-29 11:51:12 +0500

    first pattern

--- src/qa/main/patterns.java
+++ src/qa/main/patterns.java
@@ -5,17 +5,38 @@ public class patterns {
 //    first
 
     /*
-    * *****
-    * ****
-    * ***
-    * **
     * *
+    * **
+    * ***
+    * ****
+    * *****
     */
 
+    public void firstPattern(){
+
+        int val = 5;
+
+        for (int i = 1; i <= val; i++) {
+
+            for (int j = 0; j < i; j++) {
+
+                System.out.print("*");
+
+            }
+
+            System.out.println(" ");
+
+        }
+
+    }
 
 
 
     public static void main(String[] args) {
+
+        patterns obj = new patterns();
+
+        obj.firstPattern();
 
     }
 }
