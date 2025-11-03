bcebef010313c2548b9195e53fcb294796e15297
umairboota <mumairboota007@gmail.com>
2025-11-03 18:35:18 +0500

    started learning the strings

---
+++ src/qa/strings/StringLiterals.java
@@ -0,0 +1,20 @@
+package qa.strings;
+
+public class StringLiterals {
+	
+	
+	
+	
+
+	
+	public static void main(String[] args) {
+		
+		
+		String str = "Umair";
+		
+		System.out.println(str);
+		
+		
+	}
+
+}
