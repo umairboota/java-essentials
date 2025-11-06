dd061d77db1b3f0c0bd4aa91dc647d211e99a6ef
umairboota <mumairboota007@gmail.com>
2025-11-06 16:06:22 +0500

    started learning insertion sort and also added some description for the bubble sorting.

--- src/qa/main/ArrayIsSorted.java
+++ src/qa/main/ArrayIsSorted.java
@@ -13,7 +13,6 @@ public class ArrayIsSorted {
 		return true;
 	}
 
-	
 //	insertion sort
 	
 	public void sortInsertionArray(int[] arr) {
@@ -28,10 +27,19 @@ public class ArrayIsSorted {
 
 //        soring algo
 /*
+Insertion sort is based on the idea that one element from the input elements is
+consumed in each iteration to find its correct position i.e, the position to which
+it belongs in a sorted array.
 
+It iterates the input elements by growing the sorted array at each iteration.
+It compares the current element with the largest value in the sorted array.
+If the current element is greater, then it leaves the element in its place and moves on to the next element else it
+finds its correct position in the sorted array and moves it to that position.
+This is done by shifting all the elements, which are larger than the current element,
+in the sorted array to one position ahead.
 
-
-
+Time Complexity:
+                O(n^2)
 
 
 */
