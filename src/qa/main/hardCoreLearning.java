d6ff615efb61a298b202c1b20d9b987854de0a1d
Muhammad Umair <mumairboota007@gmail.com>
2026-06-26 23:05:31 +0500

    started learning arrays

---
+++ src/qa/main/hardCoreLearning.java
@@ -0,0 +1,59 @@
+package qa.main;
+
+import java.util.Arrays;
+
+public class hardCoreLearning {
+
+
+//    problems
+
+    /*
+    Largest/smallest element
+    Second largest
+    Reverse array
+    Sum and average
+    Count even/odd
+    Remove duplicates
+    Find duplicates
+    Missing number
+    Frequency of elements
+    Common elements
+    Merge arrays
+    Rotate array
+    Move zeros
+    Binary search
+    Two Sum
+    Kadane's algorithm
+     */
+
+
+    int[] arr = {34, 78, 89, 23, 0, -1, 46, 3, 90};
+
+
+//    1. Find the largest element
+
+    public void largetNumber() {
+
+        int max = arr[0];
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (max < arr[i]) {
+                max = arr[i];
+            }
+
+        }
+        System.out.println(max);
+
+    }
+
+
+    public static void main(String[] args) {
+
+//        problem 1
+        hardCoreLearning hardCodedLearning = new hardCoreLearning();
+
+        hardCodedLearning.largetNumber();
+
+    }
+}
--- src/qa/main/maxElementInAnArray.java
+++ src/qa/main/maxElementInAnArray.java
@@ -4,13 +4,12 @@ public class maxElementInAnArray {
     void maxElement(){
 
 
-        int[] arr = {5, 8, 9, 25, 78, 63, 1};
+        int[] arr = {5, 8, 9, 25,-9, 78, 63, 1, 200};
         int max = arr[0];
 
         for (int i = 0; i < arr.length; i++) {
 
             if (arr[i] > max) {
-
                 max = arr[i];
             }
 
