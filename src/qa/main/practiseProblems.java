6696ecf3d71ef1a198b301e3a077a5aab74f8750
Muhammad Umair <mumairboota007@gmail.com>
2026-07-12 23:50:56 +0500

    practice

---
+++ src/qa/main/practiseProblems.java
@@ -0,0 +1,16 @@
+package qa.main;
+
+public class practiseProblems {
+
+    int arr[] = {2, 4, 8, 0, 4, -4, 3};
+
+
+    public void smallElement(){
+        int small = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (small > )
+        }
+    }
+
+
+}
