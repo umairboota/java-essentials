d814f24fcf17835c47f05b47e69a87b1f2d6f4bf
Muhammad Umair <mumairboota007@gmail.com>
2026-07-12 23:54:08 +0500

    practice

--- src/qa/main/practiseProblems.java
+++ src/qa/main/practiseProblems.java
@@ -1,15 +1,22 @@
 package qa.main;
 
 public class practiseProblems {
+    int[] arr = {2, 4, 8, 0, 4, -4, 3};
 
-    int arr[] = {2, 4, 8, 0, 4, -4, 3};
 
-
-    public void smallElement(){
+    public int smallElement() {
         int small = arr[0];
         for (int i = 0; i < arr.length; i++) {
-            if (small > )
+            if (arr[i] < small) {
+                small = arr[i];
+            }
         }
+        return small;
+    }
+
+    public static void main(String[] args) {
+        practiseProblems obj = new practiseProblems();
+        System.out.println(obj.smallElement());
     }
 
 
