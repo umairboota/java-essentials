77800791176250d87d040981658a7dd26f966fcd
Muhammad Umair <mumairboota007@gmail.com>
2026-07-15 23:04:43 +0500

    learning arrays, strings are next

--- src/qa/main/hardCoreLearning.java
+++ src/qa/main/hardCoreLearning.java
@@ -166,7 +166,13 @@ public class hardCoreLearning {
 
     int[] newArr = {5, 7, 44, 66, 55, 77, 8, 5, 5, 5, 0, -5};
 
+    public int[] removeDuplicates(){
 
+        
+
+
+        return newArr;
+    }
 
     public static void main(String[] args) {
 
