21832486bdb2134c83434fb4b7d461f09fa39b09
umairboota <mumairboota007@gmail.com>
2026-07-21 00:56:39 +0500

    Arrays

--- src/qa/main/practiseProblems.java
+++ src/qa/main/practiseProblems.java
@@ -37,7 +37,7 @@ public class practiseProblems {
         for (int i = 0; i < arr.length/2; i++) {
             temp = arr[i];
             arr[i] = arr[arr.length-1-i];
-
+            arr[arr.length-1-i] = temp;
 
         }
 
