eef603f8afe9d4e543d6d642cd5692f7a8ce8356
umairboota <mumairboota007@gmail.com>
2026-07-21 00:55:05 +0500

    Arrays

--- src/qa/main/practiseProblems.java
+++ src/qa/main/practiseProblems.java
@@ -32,6 +32,15 @@ public class practiseProblems {
 
     public void reverseTheArray(){
 
+        int temp = arr[0];
+
+        for (int i = 0; i < arr.length/2; i++) {
+            temp = arr[i];
+            arr[i] = arr[arr.length-1-i];
+
+
+        }
+
     }
 
     public int findSecondLargeNumber() {
